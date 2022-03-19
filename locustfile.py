1c999e8d0b8064854e3ff8229e41d406bbb30fb5
Amirali53 <99325403+Amirali53@users.noreply.github.com>
2022-03-19 20:33:38 +0100

    Update locustfile.py

--- locustfile.py
+++ locustfile.py
@@ -1,7 +1,7 @@
 from locust import HttpUser, task, between
 
 class WebsiteTestUser(HttpUser):
-    wait_time = between(0.5, 3.0)
+    wait_time = between(0.5, 30.0)
 
     @task(1)
     def test1(self):
